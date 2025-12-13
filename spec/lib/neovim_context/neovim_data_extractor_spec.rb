53f92a8bd7571e822b868129baa3d771e123fd8d
Mathias Jean Johansen <mathias@mjj.io>
2025-12-13 11:48:10 +0100

    Move `allow` calls to `before` block.

--- spec/lib/neovim_context/neovim_data_extractor_spec.rb
+++ spec/lib/neovim_context/neovim_data_extractor_spec.rb
@@ -9,10 +9,13 @@ RSpec.describe NeovimContext::NeovimDataExtractor do
     let(:current) { instance_double(Neovim::Current) }
     let(:window) { instance_double(Neovim::Window) }
 
-    it "returns the cursor position as a hash" do
+    before do
       allow(client).to receive(:current).and_return(current)
       allow(current).to receive(:window).and_return(window)
       allow(window).to receive(:cursor).and_return([1, 0])
+    end
+
+    it "returns the cursor position as a hash" do
       expect(described_class.cursor_info(client: client)).to eq({ line: 1,
                                                                   col: 0 })
     end
@@ -22,10 +25,13 @@ RSpec.describe NeovimContext::NeovimDataExtractor do
     let(:current) { instance_double(Neovim::Current) }
     let(:buffer) { instance_double(Neovim::Buffer) }
 
-    it "returns the current file name" do
+    before do
       allow(client).to receive(:current).and_return(current)
       allow(current).to receive(:buffer).and_return(buffer)
       allow(buffer).to receive(:name).and_return("/path/to/file.rb")
+    end
+
+    it "returns the current file name" do
       expect(described_class.file_info(client: client)).to eq(
         "/path/to/file.rb"
       )
