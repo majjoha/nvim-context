5e415c3e427707bba6afd58f3d000cb4b315fb74
Mathias Jean Johansen <mathias@mjj.io>
2025-12-13 14:07:50 +0100

    Remove unnecessary `require "neovim"`.

--- lib/neovim_context/context_builder.rb
+++ lib/neovim_context/context_builder.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "neovim"
-
 module NeovimContext
   class ContextBuilder
     def self.build(client:)
--- lib/neovim_context/neovim_data_extractor.rb
+++ lib/neovim_context/neovim_data_extractor.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "neovim"
-
 module NeovimContext
   class NeovimDataExtractor
     def self.cursor_info(client:)
